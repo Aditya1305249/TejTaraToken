bd3361bf154a2b1005e6d7793a0065dc0a050544
Aditya1305249 <adityamohan1994@gmail.com>
2019-08-09 07:54:48 +0530

    buyToken completed

--- contracts/TejTaraTokenSale.sol
+++ contracts/TejTaraTokenSale.sol
@@ -6,10 +6,31 @@ contract  TejTaraTokenSale {
     address admin;
     TejTaraToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokenSold;
+
+
+    event Sell(address buyer,uint256 amount);
 
     constructor( TejTaraToken _address,uint256 _tokenPrice) public {
         admin=msg.sender;
         tokenContract= _address;
         tokenPrice = _tokenPrice;
     }
+
+    function multiply(uint256 x,uint256 y) internal pure returns(uint256 z) {
+        require(y==0 || (z= x*y)/y==x);
+    }
+  
+
+    function buyTokens(uint256 numberOfTokens)public payable {
+       require(msg.value == multiply(numberOfTokens,tokenPrice));
+       require(tokenContract.balanceOf(address(this))>=numberOfTokens);
+       require(tokenContract.transfer(msg.sender,numberOfTokens));
+
+       tokenSold += numberOfTokens;
+
+       emit Sell(msg.sender,numberOfTokens);
+
+    }
+
 } 
--- test/test_TejTaraToken_sale.js
+++ test/test_TejTaraToken_sale.js
@@ -1,22 +1,68 @@
+const TejTaraToken = artifacts.require("TejTaraToken");
 const TejTaraTokenSale = artifacts.require("TejTaraTokenSale");
 
+
 contract('TejTaraTokenSale',accounts=>{
-  let contractInstance;
+  let tokenSaleInstance;
+  let tokenInstance;
   const tokenPrice = 1000000000000000;
-    it('intiazling the contract', ()=>{
+  const admin=accounts[0];
+  const buyer=accounts[1];
+  const tokensAvailable = 750000;
+  const numberOfTokens=10;
+    it('intiazling the contract with the correct value', ()=>{    
         return TejTaraTokenSale.deployed().then(instance=>{
-            contractInstance=instance;
-        return  contractInstance.adddress;   
+            tokenSaleInstance=instance;
+        return  tokenSaleInstance.adddress;   
         }).then(address=>{
 
             assert.notEqual(address,0x0,'contract address');
-            return contractInstance.tokenContract()
+            return tokenSaleInstance.tokenContract()
         }).then(address=>{
             assert.notEqual(address,0x0,'token contract address')
-            return contractInstance.tokenPrice();
+            return tokenSaleInstance.tokenPrice();
         }).then(price=>{
             assert.equal(price,tokenPrice,'price of each token')
         })
+    })
+
+
+
+   it('facilitates tokens buying',()=>{
+    
+    return TejTaraToken.deployed().then(instance=>{
+        //token instance
+        tokenInstance=instance;
+       return TejTaraTokenSale.deployed();
+       }).then(instance=>{
+           //token sale instance
+           tokenSaleInstance=instance;
+       //provision 75% of token to token sale
+       return tokenInstance.transfer(tokenSaleInstance.address,tokensAvailable,{from:admin})
+       }).then(receipt=>{
+        return tokenSaleInstance.buyTokens(numberOfTokens,{from:buyer,value:numberOfTokens*tokenPrice})   
+       }).then(receipt=>{
+            assert.equal(receipt.logs.length,1,'only 1 event triggered');
+            assert.equal(receipt.logs[0].event,'Sell','event name ');
+            assert.equal(receipt.logs[0].args.buyer,buyer,'logs the account that purchase the tokens')
+            assert.equal(receipt.logs[0].args.amount,numberOfTokens,'number of token sell')
+           return tokenSaleInstance.tokenSold()
+       }).then(balance=>{
+           assert.equal(balance,numberOfTokens,'number of tokens sold');
+            return tokenInstance.balanceOf(buyer);
+       }).then(balance=> {
+           assert.equal(balance,numberOfTokens,'balance of buyer') 
+           return tokenInstance.balanceOf(tokenSaleInstance.address);
+        }).then(balance=> {
+            assert.equal(balance,tokensAvailable-numberOfTokens,'balance of buyer') 
+           return tokenSaleInstance.buyTokens(numberOfTokens,{from:buyer,value:1})
+          }).then(assert.fail).catch(error=>{
+           assert(error.message.indexOf('revert')>=0,'buying token with less value');
+          return tokenSaleInstance.buyTokens(8000000,{from:buyer,value:numberOfTokens*tokenPrice})
+         }).then(assert.fail).catch(error=>{
+           assert(error.message.indexOf('revert')>=0,'buying token more  than available');
+  
+     })
     })
 
 
